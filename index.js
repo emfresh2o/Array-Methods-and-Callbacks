f27bd1730f8c94db51b54665934ebd3ba42a55a5
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-06-09 22:56:37 -0400

    final changes

--- index.js
+++ index.js
@@ -41,16 +41,16 @@ console.log(winner14[0]['Home Team Name']);
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(data) {
-    return data.filter((final) => final.Stage === 'Finals');
+    return data.filter((final) => final.Stage === 'Final');
 };
 
 console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(cb) {
-   
-    return cb.map((game) => final.Year);
+function getYears(callback) {
+    const yearArr = callback.map((game) => game.Year);
+    return yearArr;
     
 }
 
